49a521bf16bb520c6eb5a00d739b0b71d04924b2
jini29 <60157945+jini29@users.noreply.github.com>
2024-03-10 18:40:50 -0500

    set up spring-boot template for backend and connected with gcp bigTable and deployed to app enigine

---
+++ comp539_team2_backend/src/main/java/com/example/comp539_team2_backend/Comp539Team2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.comp539_team2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp539Team2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp539Team2BackendApplication.class, args);
+    }
+
+}
